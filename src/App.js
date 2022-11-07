8c648ffc5e8aedd7adf947fb29be3d18e42fb1d6
Nguyên Dương <102743853+nguyengz@users.noreply.github.com>
2022-11-08 00:01:17 +0700

    LoginTest

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Login from './pages/Login.js';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
